7c8a37ef2f80d90812002f1b7575c15d041b699f
robinschapiro <144143314+robinschapiro@users.noreply.github.com>
2023-10-17 16:32:29 +0200

    Add files via upload

---
+++ calculatrice.js
@@ -0,0 +1,40 @@
+function addition(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)+Number(b);
+
+    t3.value = c;
+}
+function soustraction(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)-Number(b);
+
+    t3.value = c;
+}
+function division(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)/Number(b);
+
+    t3.value = c;
+}
+function multiplication(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)*Number(b);
+
+    t3.value = c;
+
+}
+
+function parite(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+}
+
+function permut(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+
+}
