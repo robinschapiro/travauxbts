b9f34ea0224fa710575d7385d18ad962e2e6e318
robinschapiro <144143314+robinschapiro@users.noreply.github.com>
2023-11-14 16:52:07 +0100

    Add files via upload

---
+++ facture.js
@@ -0,0 +1,20 @@
+function Facture() {
+    var a =document.getElementById("t1").value;
+    var b =document.getElementById("t2").value;
+    var c = Number(a)+Number(b);
+    t3.value= c ; 
+
+    var d =document.getElementById("t1").value;
+    var e =document.getElementById("t2").value;
+    var f = Number(d)+Number(e);
+    t3.value= c ;
+
+    var g =document.getElementById("t1").value;
+    var h =document.getElementById("t2").value;
+    var j = Number(g)+Number(h);
+    t3.value= c ;
+
+    var somme = Number(k)+Number(c)+Number(f);
+    t3.value=(somme)
+
+}
